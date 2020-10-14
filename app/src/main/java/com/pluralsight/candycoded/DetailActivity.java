51e373b35ee4f5107d3d6615463cf4871de11780
Terence Vusile Silonda <vusile@gmail.com>
2020-10-15 02:08:38 +0300

    Done

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,7 +19,7 @@ import com.squareup.picasso.Picasso;
 public class DetailActivity extends AppCompatActivity {
 
     public static final String SHARE_DESCRIPTION = "Look at this delicious candy from Candy Coded - ";
-    public static final String HASHTAG_CANDYCODED = " #candycoded";
+    public static final String HASHTAG_CANDYCODE = " #candycoded";
     String mCandyImageUrl = "";
 
     @Override
@@ -71,4 +72,19 @@ public class DetailActivity extends AppCompatActivity {
     // ***
     // TODO - Task 4 - Share the Current Candy with an Intent
     // ***
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        createShareIntent();
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void createShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODE);
+        startActivity(shareIntent);
+    }
 }
